6704314cc930083069eb2771172e8992af9b06e2
Shaar <ss.f@tutanota.com>
2022-07-28 12:29:01 -0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const app = express();
-
-app.get('/', (req, res) => {
-    res.send('Hello World');
+const mysql = require('mysql');
+ 
+const connection=mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'password',
+    database:'babylon'
 });
+ 
+connection.connect(function(error){
+    if(!!error) console.log(error);
+    else console.log('SQL Database Connected!');
+}); 
+
 
 app.listen(3001, () => {
     console.log('Listening on Port 3001.');
